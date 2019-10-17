78f45be2a96623f8c59f4fc8e1d16b80a6ae1bdd
danushka96 <danushkaherath96@gmail.com>
2019-10-17 22:58:28 +0530

    add basic imple

---
+++ src/components/sideNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="4" sm="4" style="margin-left:unset;">
+      <v-card height="680" width="256" class="mx-auto my-12" style="margin-left: unset !important;">
+        <v-navigation-drawer permanent>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title" style="margin-top: 20px !important;">Application</v-list-item-title>
+              <v-list-item-subtitle>subtext</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list dense nav>
+            <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "sideNavigation",
+  data: () => ({
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard" },
+      { title: "Photos", icon: "mdi-image" },
+      { title: "About", icon: "mdi-help-box" }
+    ],
+    right: null
+  })
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,37 @@
 <template>
-  <HelloWorld />
+  <v-row no-gutters>
+    <v-col cols="4" sm="4" style="margin-left:unset;">
+      <!-- <sideNavigation/> -->
+    </v-col>
+    <v-col cols="8" sm="4" style="margin-left:unset;">
+      <!-- <companyDetails/> -->
+      <HelloWorld/>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import { ALL_COMPANY_DETAILS } from "../graphql";
+// import { sideNavigation } from '../components/sideNavigation';
+// import { companyDetails } from '../components/CompanyDetails';
+import { HelloWorld } from '../components/HelloWorld';
 
 export default {
+  name: "Home",
   components: {
-    HelloWorld,
+    // sideNavigation,
+    // companyDetails,
+    HelloWorld
   },
+  data() {
+    return {
+      company: []
+    };
+  },
+  apollo: {
+    company: {
+      query: ALL_COMPANY_DETAILS
+    }
+  }
 };
 </script>
